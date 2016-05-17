40f4ac33f3f1569c94fb143b580dbc749ebd2a42
Diogo Silva <dbtds@ua.pt>
2016-05-17 13:13:27 +0100

    Small changes to svm

--- src/support-vector-machine/svm-main.m
+++ src/support-vector-machine/svm-main.m
@@ -2,3 +2,5 @@
 % Email Address: dbtds@ua.pt
 
 data = csvread('data/car_indexed.data'); 
+# You must compile libsvm modules before with 'make octave'
+addpath('support-vector-machine/libsvm-3.21/matlab/')
