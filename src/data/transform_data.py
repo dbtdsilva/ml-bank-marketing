30c5ab59b3d81f262b81d68a92d0eec9c6dffe30
Diogo Silva <dbtds@ua.pt>
2016-05-27 15:00:59 +0100

    Change files from car evaluation to bank marketing and adapted script

--- src/data/transform_data.py
+++ src/data/transform_data.py
@@ -1,9 +1,11 @@
 import csv
 
-def get_csv_data(filename, fieldnames):
+def get_csv_data(filename, fieldnames, has_header=True):
     data = []
     with open(filename) as csvfile:
-        reader = csv.DictReader(csvfile, fieldnames=fieldnames)
+        if has_header:
+            csvfile.readline()
+        reader = csv.DictReader(csvfile, delimiter=';', fieldnames=fieldnames)
         for entry in reader:
             data += [ entry ]
     return data
@@ -13,7 +15,7 @@ def get_indexed_data(data, enums):
     for idx, entry in enumerate(data):
         new_entry = {}
         for (key, val) in entry.items():
-            new_entry[key] = enums[key].index(val)
+            new_entry[key] = enums[key].index(val) if key in enums else val
         data_indexed += [ new_entry ]
     return data_indexed
 
@@ -34,20 +36,28 @@ def export_new_csv(filename, keys, data):
     f.close()
 
 def main():
-    keys = ('buying', 'maint', 'doors', 'persons', 'lug_boot', 'safety', 'class')
+    keys = ("age","job","marital","education","default","housing","loan",
+        "contact","month","day_of_week","duration","campaign","pdays",
+        "previous","poutcome","emp.var.rate","cons.price.idx",
+        "cons.conf.idx","euribor3m","nr.employed","y")
+
     enums = {
-        'buying' : ['vhigh', 'high', 'med', 'low'],
-        'maint' : ['vhigh', 'high', 'med', 'low'],
-        'class': ['unacc', 'acc', 'good', 'vgood'],
-        'doors' : ['2', '3', '4', '5more'],
-        'persons' : ['2', '4', 'more'],
-        'lug_boot' : ['small', 'med', 'big'],
-        'safety' : ['low', 'med', 'high']
+        'job' : ['admin.','blue-collar','entrepreneur','housemaid','management','retired','self-employed','services','student','technician','unemployed','unknown'],
+        'marital' : ['divorced','married','single','unknown'],
+        'education' : ['basic.4y','basic.6y','basic.9y','high.school','illiterate','professional.course','university.degree','unknown'],
+        'default' : ['no','yes','unknown'],
+        'housing' : ['no','yes','unknown'],
+        'loan' : ['no','yes','unknown'],
+        'contact' : ['cellular','telephone'],
+        'month' : ['jan','feb','mar','apr','may','jun','jul','aug','sep','oct','nov','dec'],
+        'day_of_week' : ['mon','tue','wed','thu','fri'],
+        'poutcome' : ['failure','nonexistent','success'],
+        'y' : ['yes','no']
     }
 
-    data = get_csv_data('car.data', keys)
+    data = get_csv_data('bank-additional.csv', keys)
     data_indexed = get_indexed_data(data, enums)
-    export_new_csv('car_indexed.data', keys, data_indexed)
+    export_new_csv('bank-fixed.csv', keys, data_indexed)
     # Load using Octave
     # data = csvread('car_indexed.data')
     #print(get_raw_array(keys, data_indexed))
