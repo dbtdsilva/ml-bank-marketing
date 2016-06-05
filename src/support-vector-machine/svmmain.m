8abaf5ea657a4af45c7079eb6b13bd3a6b640a7e
Diogo Silva <dbtds@ua.pt>
2016-06-05 19:04:07 +0100

    Cleanup code

--- src/support-vector-machine/svmmain.m
+++ src/support-vector-machine/svmmain.m
@@ -8,77 +8,70 @@ colorstring = 'rbgry';
 %scatter(data(:, 1), data(:, 2), colorClass(data(:,7) + 1))
 
 X = data(:,1:end-1);
-Y = not(data(:, end));
+Y = not(data(:, end)); % inverting -> yes is 1 and no is 0
 
 % 
-% %% TRYING PCA ANALYSIS
-% Xfiltered = X;
-% %Xfiltered(:,10:20) = [];
-% %Xfiltered(:,1:5) = [];
-% %Xfiltered(:,11) = [];
-% 
-% [coeff,score,latent,tsquared] = pca(Xfiltered, 'NumComponents', 2);
-% Xcentered = score*coeff';
-% 
-% % 11, 13, 20 são bosta... clean
-% %
-% %cmap = [1 0 0; 0 1 0; 0 0 1];
-% %colormap(cmap);
-% %scatter(score(:,1), score(:,2), 40, Y);
-% 
-%% APPLYING CROSS VALIDATION
-crossvalid = 7;
+%% TRYING PCA ANALYSIS
+Xfiltered = X;
+ 
+[coeff,score,latent,tsquared] = pca(Xfiltered, 'NumComponents', 2);
+Xcentered = score*coeff';
 
-% duration, pdays, poutcome (12, 14, 16)
-% http://www.ijeat.org/attachments/File/v4i4/D3963044415.pdf
+cmap = [1 0 0; 0 1 0; 0 0 1];
+colormap(cmap);
+scatter(score(:,1), score(:,2), 40, Y);
+ 
 
-
-global gama 
-global sigma
-global polyorder
-gama = 1;
-sigma = 2.47;
-polyorder = 3;
-
+%% Normalization every feature value from 0 to 1
 minX = min(X);
 rangeX = max(X)-minX;
 lX = length(X);
 Xnorm = (X - repmat(minX, lX, 1)) ./ repmat(rangeX, lX, 1);
 
-%for gama = 1:0.1:5
+%% Algorithm to train, predict and check the performane
+% Run for different scales (it depends from the kernel used):
+% linear -> no kernel scaling (it will make no difference)
+% polynomial -> gamma
+% rbf -> gamma 
+stats_mean = [];
+%algorithms = {{'linear', 0, }, {'polynomial', 20}, 'rbf'};
+for kernelScale = 1:0.1:20
+    %% APPLYING CROSS VALIDATION
     stats = zeros(crossvalid, 4);
+    crossvalid = 7;
     for index = 1:crossvalid
         %% COMPUTING SVM
         [ TrainX, TestX ] = splitset(Xnorm, index, crossvalid);
         [ TrainY, TestY ] = splitset(Y, index, crossvalid);
         fprintf('Training.. ');
         %fitcsvm(TrainX, TrainY, 'Standardize', true, 'KernelFunction', 'mrbf')
+        prior0 = mean(TrainY);
+        prior1 = 1 - prior0;
+        
+        % Measuring the performance time
+        tic
         svmModel = fitcsvm(TrainX, TrainY, ...
             'CategoricalPredictors', [2,3,4,5,6,7,8,9,10,15], ... % additional set
             'Standardize', true, ...
-            'KernelFunction', 'rbf', ...
-            'Cost', [0,1.72;1,0], ...            % cost of misclassify
-            'KernelScale', sigma, ...
-            'Prior', [0.2 0.3], ...%'uniform', ...       % empirical, uniform, ...
-            'Solver', 'SMO' ...           % ISDA (w Outl), L1QP, SMO (w/o Outl)
+            'KernelFunction', 'linear', ...
+            ... %'Cost', [0,1.72;1,0], ...            % cost of misclassify
+            'KernelScale', kernelScale, ...
+            'Prior', [prior0 prior1], ...       % empirical, uniform, ...
             ... %'PolynomialOrder', 3, ...       % default is 3
-            ... %'KernelOffset', 0, ...          % 0 for SMO, 0.1 ISDA
-            ... %'KKTTolerance', 0, ...          % defaults: 0 SMO, 1e-3 ISDA
-            ... %'ScoreTransform', 'doublelogit', ...   % 'none'-(default),'doublelogit',
-            ... %'invlogit','ismax','logit','sign','symmetric','symmetriclogit','symmetricismax'
-            ... %'DeltaGradientTolerance', 0, ...% defaults: 0 ISDA, 1e-3 SM
-         );
-
+            'KernelOffset', 0 ...          % 0 for SMO, 0.1 ISDA
+        );
+        trainTiming = toc
         % poly, SMO
         fprintf('Trained! Predicting.. ');
+        tic
         out = predict(svmModel, TestX);
+        predictTiming = toc
         fprintf('Predicted! ');
         %% COMPUTING FMEASURE
         [acc, fscore, recall, precision] = fmeasure(TestY, out);
-        stats(index, :) = [fscore, acc, recall, precision];
+        stats(index, :) = [fscore, acc, recall, precision, trainTiming, predictTiming];
         fprintf('F-Measure: %.4f, Accuracy: %.4f, Recall: %.4f, Precision: %.4f\n', fscore, acc, recall, precision)
     end
-    stats_mean = [sigma mean(stats)]
-%end
-%stats_std = std(stats)
+    stats_mean(end+1,:) = [kernelScale mean(stats)]
+end
 
