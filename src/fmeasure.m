ac20244178ec4704ce6a2739e198b3b5f18485a7
Diogo Silva <dbtds@ua.pt>
2016-06-05 15:30:15 +0100

    ups..

--- src/fmeasure.m
+++ src/fmeasure.m
@@ -12,14 +12,14 @@ function [acc, fscore, recall, precision] = fmeasure(Y, Ypred)
     acc = (true_positives + true_negatives) / (true_positives + true_negatives + false_positives + false_negatives);
     if true_positives + false_negatives == 0
         recall = 0;
-    else
-        recall = true_positives / (true_positives + false_negatives)
+    else,
+        recall = true_positives / (true_positives + false_negatives);
     end;
 
     if true_positives + false_positives == 0
         precision = 0;
     else
-        precision = true_positives / (true_positives + false_positives)
+        precision = true_positives / (true_positives + false_positives);
     end;
 
     if recall + precision == 0
